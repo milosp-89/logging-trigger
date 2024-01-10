9659b577916f4fa164b83c9b2b08b2f9b0c12be9
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-01-10 10:45:10 +0100

    Create logging_tbl.sql

---
+++ logging_tbl.sql
@@ -0,0 +1,11 @@
+use [xxx]
+go;
+
+create table [dbo].[logging_tbl](
+	[Table_Name] [nvarchar](50) null,
+	[SubmissionID] [nvarchar](50) null,
+	[ModifiedBy] [varchar](25) null,
+	[ModifiedOn] [datetime] null,
+	[Change] [char](10) null
+)
+go;
